501bbec216a111abb3a5f37d1bff82d96b093bc4
Emmanuel Murano <emmanuelm41@gmail.com>
2020-10-23 01:27:00 +0200

    create client tests

--- platform/oasis/client_test.go
+++ platform/oasis/client_test.go
@@ -1,11 +1,62 @@
 package oasis
 
-import "testing"
+import (
+	"os"
+	"testing"
+
+	"github.com/hasura/go-graphql-client"
+)
 
 const (
-	inputPk         = ""
-	expectedAddress = ""
+	EnvNameGraphQlURL = "GRAPHQL_URL"
 )
+
+const (
+	inputPk           = ""
+	expectedAddress   = ""
+	graphqlDefaultURL = "http://127.0.0.1:8080/v1/graphql"
+)
+
+func TestGetCurrentBlock(t *testing.T) {
+
+	url := os.Getenv(EnvNameGraphQlURL)
+	if url == "" {
+		url = graphqlDefaultURL
+	}
+
+	client := Client{graphqlClient: graphql.NewClient(url, nil)}
+
+	height, err := client.GetCurrentBlock()
+
+	if err != nil {
+		t.Errorf("The block %d was not fetched: %v \n", height, err)
+	}
+
+	t.Logf("The fetched height block is %d", height)
+
+}
+
+func TestGetBlockByNumber(t *testing.T) {
+
+	url := os.Getenv(EnvNameGraphQlURL)
+	if url == "" {
+		url = graphqlDefaultURL
+	}
+
+	height := int64(1)
+	client := Client{graphqlClient: graphql.NewClient(url, nil)}
+
+	blk, err := client.GetBlockByNumber(height)
+
+	if err != nil {
+		t.Errorf("The block %d was not fetched: %v \n", height, err)
+	}
+
+	if int64(blk.Height) != height {
+		t.Error("The block fetched is not the correct one\n")
+	}
+
+}
 
 func TestGetAddressesFromXpub(t *testing.T) {
 
